359f7f4f6e7cc814b9a5939a5f968550ae5730a9
Alexey Popov <alexeypopov183@gmail.com>
2022-04-24 13:04:00 +0300

    feat: styles for message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -20,8 +20,21 @@ const Message = ({messageProp, currentUser}) => {
 
   return (
     <div className="message-block">
-      {userName}: {message} <span style={{color: "red"}} onClick={() => deletedMessage(uniqId)}>x</span>
-      {encodeImage && <img className="message-block__img" src={encodeImage} alt="image"/>}
+      <div className="message-block__title">
+        {userName}
+      </div>
+      <div className="message-block__subtitle">
+        <p>{message}</p>
+        {encodeImage && <img className="message-block__subtitle-img" src={encodeImage} alt="image"/>}
+
+        <div className="message-block__subtitle-close" onClick={() => deletedMessage(uniqId)}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
+            <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
+          </svg>
+        </div>
+
+      </div>
     </div>
   );
 };
